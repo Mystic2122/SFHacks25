d2c6f187fa35f3365cf38de6fca1185de7750836
Mystic2 <nickwendt7@gmail.com>
2025-04-04 20:49:50 -0700

    going home

--- bioAPI.py
+++ bioAPI.py
@@ -9,11 +9,11 @@ load_dotenv()
 
 api_key = os.getenv("BIO_API")
 player_id = "8ec91366-faea-4196-bbfd-b8fab7434795"
-
-url = f"https://api.sportradar.com/nba/trial/v7/en/players/{player_id}/profile.json?api_key={api_key}"
+# /{player_id}/profile
+url = f"https://api.sportradar.com/nba/trial/v7/en/players.json?api_key={api_key}"
 
 headers = {"accept": "application/json"}
 
 response = requests.get(url, headers=headers)
 data = response.json()
-print(data["college"])
+print(data)
--- real.py
+++ real.py
@@ -12,3 +12,12 @@ for team in teams_data["teams"]:
     team_id.append(team["id"])
 
 print(team_id)
+
+player_id = []
+for team in team_id:
+    url = "https://api.sportradar.com/nba/trial/v7/en/teams/{team_id}/profile.json?api_key=uujz5UCqJ2Y8hrU5VnpF6mC23cROB2bFFKBSz4Qa"
+    headers = {"accept": "application/json"}
+    response = requests.get(url, headers=headers)
+    roster_data = response.json()
+    for player in roster_data["players"]:
+        player_id.append(player["id"])
