ce2e2a46b30979ec1b343295701ae1ded53802b4
Mystic2 <nickwendt7@gmail.com>
2025-04-04 19:38:08 -0700

    davidson!!!

--- bioAPI.py
+++ bioAPI.py
@@ -8,10 +8,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 api_key = os.getenv("BIO_API")
+player_id = "8ec91366-faea-4196-bbfd-b8fab7434795"
 
-url = f"https://api.sportradar.com/nba/trial/v7/en/players/8ec91366-faea-4196-bbfd-b8fab7434795/profile.json?api_key={api_key}"
+url = f"https://api.sportradar.com/nba/trial/v7/en/players/{player_id}/profile.json?api_key={api_key}"
 
 headers = {"accept": "application/json"}
 
 response = requests.get(url, headers=headers)
-print(response.text)
+data = response.json()
+print(data["college"])
