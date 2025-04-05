74a0a593a6cb0feb82b8945188611c54228894f3
Mystic2 <nickwendt7@gmail.com>
2025-04-05 00:43:35 -0700

    oops

--- player_test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://api.sportradar.com/nba/trial/v7/en/players/8ec91366-faea-4196-bbfd-b8fab7434795/profile.json?api_key=uujz5UCqJ2Y8hrU5VnpF6mC23cROB2bFFKBSz4Qa"
-
-headers = {"accept": "application/json"}
-
-response = requests.get(url, headers=headers)
-
-print(response.text)
