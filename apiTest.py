5ff9fdf112aaa6082df805527c685f01ceaa0ae8
Mystic2 <nickwendt7@gmail.com>
2025-04-04 18:44:32 -0700

    got teams in order

--- apiTest.py
+++ apiTest.py
@@ -1,7 +1,13 @@
 import requests
 
-url = "http://rest.nbaapi.com/api/PlayerDataAdvanced/name/Lebron"
+first_last_name = "Lebron James"
+
+url = f"http://rest.nbaapi.com/api/PlayerDataAdvanced/name/{first_last_name}"
+
 
 response = requests.get(url)
+data = response.json()
+sorted_data = sorted(data, key=lambda x: x["age"])
 
-print(response.json()[-1])
+for season in sorted_data:
+    print(season["team"])
